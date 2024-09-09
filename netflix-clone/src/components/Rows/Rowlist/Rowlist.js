93a991b55dd6e9c4d6aea870c2262177501e6c25
David-Gizaw <bama.internet@gmail.com>
2024-09-09 21:29:02 +0300

    Rows finished

--- netflix-clone/src/components/Rows/Rowlist/Rowlist.js
+++ netflix-clone/src/components/Rows/Rowlist/Rowlist.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Row from "../Row/Row";
+import requests from "../../../utils/requests";
 
-const Rowlist = () => {
+const RowList = () => {
   return (
-    <div>Rowlist</div>
-  )
-}
+    <>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
 
-export default Rowlist
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="TV Shows" fetchUrl={requests.fetchTvShow} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </>
+  );
+};
+
+export default RowList;
